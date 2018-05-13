e1d13e8b77af842a98bbca07b7fcb4dece3e7755
jailton morais <itosoft23@hotmail.com>
2018-05-13 14:52:19 -0300

    Remoção Form default e criação do form login

--- CadastroDeUsuarios/CadastroDeUsuarios/Program.cs
+++ CadastroDeUsuarios/CadastroDeUsuarios/Program.cs
@@ -16,7 +16,7 @@ namespace CadastroDeUsuarios
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frm_Login());
         }
     }
 }
